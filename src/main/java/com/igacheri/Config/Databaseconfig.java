7fb419e170e7be516f8069407a4f9390e41d812b
idah <idah@moringaproject.com>
2023-07-31 22:17:08 +0300

    added library details for postgres under config package

--- src/main/java/com/igacheri/Config/Databaseconfig.java
+++ src/main/java/com/igacheri/Config/Databaseconfig.java
@@ -1,4 +1,7 @@
 package com.igacheri.Config;
-
+import org.sql2o.Sql2o;
 public class Databaseconfig {
+    public static Sql2o getDatabase(){
+        return new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "Wamwea2012!");
+    }
 }
