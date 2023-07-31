9e1add538cf5dce3ed13b11cb8ca704d0e13bced
idah <idah@moringaproject.com>
2023-07-31 22:43:40 +0300

    added java class for Ranger and Sighting model

--- src/main/java/com/igacheri/model/Ranger.java
+++ src/main/java/com/igacheri/model/Ranger.java
@@ -1,4 +1,27 @@
 package com.igacheri.model;
 
 public class Ranger {
+        public Ranger() {
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private int id;
+        private String name;
+
+    }
 }
