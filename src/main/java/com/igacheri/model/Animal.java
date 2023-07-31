b8035f9cf86c04ef3803221363e980ed98ef30c3
idah <idah@moringaproject.com>
2023-07-31 22:37:18 +0300

    added javaclass for animal model

--- src/main/java/com/igacheri/model/Animal.java
+++ src/main/java/com/igacheri/model/Animal.java
@@ -1,4 +1,55 @@
 package com.igacheri.model;
 
+import com.igacheri.Configuration.Age;
+import com.igacheri.Configuration.Health;
+
 public class Animal {
+    private Age age;
+    private Health health;
+    private int id;
+    private String name;
+    private boolean endangered;
+
+    public Animal() {
+    }
+
+    public Age getAge() {
+        return age;
+    }
+
+    public void setAge(Age age) {
+        this.age = age;
+    }
+
+    public Health getHealth() {
+        return health;
+    }
+
+    public void setHealth(Health health) {
+        this.health = health;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEndangered() {
+        return endangered;
+    }
+
+    public void setEndangered(boolean endangered) {
+        this.endangered = endangered;
+    }
 }
